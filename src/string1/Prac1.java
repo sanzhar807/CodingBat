43761f3e548533b47449135c312179fe55ee7da3
sanzhar807 <amanturov2471@gmail.com>
2026-01-16 10:25:28 +0700

    1
    2

---
+++ src/string1/Prac1.java
@@ -0,0 +1,7 @@
+package string1;
+
+public class Prac1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
---
+++ src/string1/Prac2.java
@@ -0,0 +1,7 @@
+package string1;
+
+public class Prac2 {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
