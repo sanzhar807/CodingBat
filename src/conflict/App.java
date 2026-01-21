5b60d10fb757cf7ab1746be4c1bcfb27abdcc6c8
sanzhar807 <amanturov2471@gmail.com>
2026-01-21 10:40:34 +0700

    update from master

--- src/conflict/App.java
+++ src/conflict/App.java
@@ -2,6 +2,6 @@ package conflict;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello from master");
     }
 }
